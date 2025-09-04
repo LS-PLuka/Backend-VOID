ec74a575fb854815384164a5e9aab9e690bd0fa7
Pedro Luka <pedrolukagt@gmail.com>
2025-09-04 15:37:58 -0300

    arrumando API usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -4,25 +4,26 @@ const prisma = new PrismaClient()
 
 // Funções CRUD Usuario
 export async function create(dados) {
-  try {
-    const usuario = await prisma.usuario.create({
-      data: {
-        nome: dados.nome,
-        email: dados.email,
-        senha: dados.senha,
-        role: dados.role || "cliente",
-      },
+    try {
+        const usuario = await prisma.usuario.create({
+        data: {
+            nome: dados.nome,
+            email: dados.email,
+            senha: dados.senha,
+            role: dados.role || "cliente",
+        },
     });
 
-    return usuario;
-  } catch (error) {
-    throw new Error("Erro ao criar usuário: " + error.message);
-  }
+        return usuario;
+    } catch (error) {
+        throw new Error("Erro ao criar usuário: " + error.message);
+    }
 }
 
 export async function list() {
     try {
         const usuarios = await prisma.usuario.findMany();
+        
         return usuarios;
     } catch (error) {
         throw new Error("Erro ao listar usuários: " + error.message);
@@ -34,6 +35,7 @@ export async function getUser(id) {
         const usuario = await prisma.usuario.findUnique({
             where: { id: Number(id) },
         });
+        
         return usuario;
     } catch (error) {
         throw new Error("Erro ao buscar usuário por ID: " + error.message);
@@ -50,6 +52,7 @@ export async function edit(id, dados) {
                 senha: dados.senha,
             }
         });
+        
         return usuario;
     } catch (error) {
         throw new Error("Erro ao editar usuário: " + error.message);
@@ -61,6 +64,7 @@ export async function remove(id) {
         const usuario = await prisma.usuario.delete({
             where: { id: Number(id) },
         });
+        
         return usuario;
     } catch (error) {
         throw new Error("Erro ao deletar usuário: " + error.message);
--- src/routers/usuarioRouter.js
+++ src/routers/usuarioRouter.js
@@ -3,7 +3,7 @@ import express from 'express'
 // Importando Controllers
 import { createUsuario } from '../controllers/usuario/createUsuarioController.js'
 import { listUsuarioController } from '../controllers/usuario/listUsuarioController.js'
-import { getByIdUsuarioController } from '../controllers/usuario/getByIdUsuario.js'
+import { getByIdUsuarioController } from '../controllers/usuario/getByIdUsuarioController.js'
 import { editUsuarioController } from '../controllers/usuario/editUsuarioController.js'
 import { deleteUsuarioController } from '../controllers/usuario/deleteUsuarioController.js'
 
