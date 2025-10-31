20231d4b6301694fac6e083520b5c5865ab06057
Pedro Luka <pedrolukagt@gmail.com>
2025-10-31 16:24:20 -0300

    passando informacoes via login

--- src/controllers/usuario/loginUsuarioController.js
+++ src/controllers/usuario/loginUsuarioController.js
@@ -19,7 +19,12 @@ export async function loginUsuarioController(req, res) {
         const payload = { id: user.id, email: user.email };
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 
-        res.json({ token });
+        res.json({
+            token,
+            id: user.id,
+            nome: user.nome,
+            email: user.email
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
